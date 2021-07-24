8aad8d6e9da37342f2c48c4ec962fe63101d146d
Firtch <firtch@gmail.com>
2021-07-25 01:33:54 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCount } from "./reducers/reposReducer";
+const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.repos.count);
 
-function App() {
+  function onCountClick() {
+    dispatch(setCount(5));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={() => onCountClick()}>COUNT</button>
+      <div>{count}</div>
     </div>
   );
-}
+};
 
 export default App;
